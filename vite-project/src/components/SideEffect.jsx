76f67ffc38afe57f5ebcabadad2b186dcee8b7d3
Shashwat Gupta <guptashashwat155@gmail.com>
2026-07-19 14:35:37 +0530

    Api fetching and rendering lists

--- vite-project/src/components/SideEffect.jsx
+++ vite-project/src/components/SideEffect.jsx
@@ -1,26 +1,29 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
 
 function SideEffect (){
      
     const[todos,setTodos] = useState([]);
-    const API = "https://jsonplaceholder.typicode.com/todos/1"
+    useEffect(()=>{
+        const API = "https://jsonplaceholder.typicode.com/todos/1"
     fetch(API) // returns a promise
     .then((resp)=>{
         return resp.json()
     }) 
-    .then((data)=>console.log(data))
+    .then((data)=>{
+        console.log(data)
+        setTodos([data]) // Modified: Wrapped 'data' in an array so .map() works, and saved it to state
+    })
     .catch((err)=>console.log(err))
+    }, []) // Modified: Added empty dependency array to stop the infinite rendering loop
     return (
         <div>
             <h1>sideeffects</h1>
-            {
-                todos.map((item,index)=>{
+            {todos.map((item)=>{
                     return(
-                        <div>
-                            <h1>{item.id} </h1>
-                            <h1>{item.title}</h1>
-                            <h1>{item.completed}</h1>
+                        <div key={item.id}> {/* Modified: Added required unique key prop */}
+                            <h1>ID:{item.id} </h1>
+                            <h1>TITLE:{item.title}</h1>
+                            <h1>COMPLETED:{item.completed.toString()}</h1> {/* Modified: Converted boolean to string so it displays */}
                         </div>
                     )
                 })
